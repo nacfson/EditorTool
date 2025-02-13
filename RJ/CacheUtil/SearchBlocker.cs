97ecf14111dfc08394c2ed2ed1c5d80b63784b9e
Yoo Hyung Joo <65493803+nacfson@users.noreply.github.com>
2025-02-13 14:26:22 +0900

    remove

--- RJ/CacheUtil/SearchBlocker.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SearchBlocker : MonoBehaviour, INotSearchable
-{
-    [field:SerializeField] public bool EnableSearchChildren { get; set; } = false;  
-}
